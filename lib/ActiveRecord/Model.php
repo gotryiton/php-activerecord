5ab9cbf46b3325f54409a0f48d445fe031cfc3eb
sbader <sb@scottbader.org>
2012-12-12 10:11:05 -0500

    adding finder functionality to model class

--- lib/ActiveRecord/Model.php
+++ lib/ActiveRecord/Model.php
@@ -82,6 +82,13 @@ class Model
 
     static $auto_load_associations_for_model = array();
 
+    /**
+     * Automatically eager load these associations within a finder
+     *
+     * @var array
+     **/
+    static $auto_load_associations_for_finder = array();
+
 	/**
 	 * Contains model values as column_name => value
 	 *
@@ -238,6 +245,46 @@ class Model
 	 * @var array
 	 */
 	static $delegate = array();
+
+    /**
+     * Named options to simplify usage of find methods and allow for easier caching of results.
+     *
+     * <code>
+     * class Person extends ActiveRecord\Model {
+     *   static $finders = array(
+     *      'by_first_name' => array(
+     *          'conditions' => array('first_name = ?', 'first_name'),
+     *          'limit' => 10,
+     *          'includes' => array('orders')
+     *      )
+     *   );
+     * }
+     * </code>
+     *
+     * usage:
+     *
+     * <code>
+     * $people = Person::finder('by_first_name', array('first_name' => 'Tito'));
+     * </code>
+     *
+     * You can add additional options in the 3rd parameter
+     *
+     * <code>
+     * $people = Person::finder('by_first_name', array('first_name' => 'Tito'), array('offset' => 10, 'limit' => 100');
+     * </code>
+     *
+     * @var array
+     **/
+    static $finders = array();
+
+    /**
+     * The default cache timeout for caching of a finder's results.
+     *
+     * Can be overriden with the `ttl` index for a specific finder.
+     *
+     * @var integer
+     **/
+    static $finder_default_ttl = 120;
 
 	/**
 	 * Constructs a model.
@@ -2129,6 +2176,355 @@ class Model
         else {
             $this->__relationships[$name] = $object;
         }
+    }
+
+    /**
+     * Takes an array of models, filters by a unique attribute and returns an array of a limited number of models
+     *
+     * @param array $objects
+     * @param string $attribute
+     * @param string $limit
+     *
+     * @return array
+     */
+    public static function unique($objects, $attribute = 'id', $limit = null) {
+        $used_attributes = array();
+        $returned_objects = array();
+        foreach ($objects as $object) {
+            $value = $object->read_attribute($attribute);
+            if (!in_array($value,$used_attributes)) {
+                $used_attributes[] = $object->read_attribute($attribute);
+                $returned_objects[] = $object;
+                if (!is_null($limit) && count($returned_objects) == $limit) {
+                    return $returned_objects;
+                }
+            }
+        }
+
+        return $returned_objects;
+    }
+
+    /**
+     * Loads options from predefined finder and runs a find on the Model
+     *
+     * Set or override conditions by setting conditions array.
+     * Set or override limit, offset, order by, etc.. by setting filters array.
+     * If an attribute appears multiple times in the conditions string set the attribute
+     * as an array with each value.
+     *
+     * @param string $name
+     * @param array $conditions
+     * @param array $filters
+     * @param bool $ignore_cache
+     *
+     * @return array of model objects
+     */
+    public static function finder($name, $conditions = array(), $filters = array(), $ignore_cache = false){
+        if (array_key_exists($name,static::$finders)) {
+            $found_objects = false;
+            if (!$ignore_cache) {
+               $specific_cache_id = self::finder_specific_cache_id($name, $conditions, $filters);
+               if ($finder = Cache::fetch($specific_cache_id)) {
+                   $default_options = static::$finders[$name];
+                   if (!array_key_exists('include', $default_options )) {
+                       $default_options['include'] = static::$auto_load_associations_for_finder;
+                   }
+                   $found_ids = $finder['ids'];
+                   $totals = $finder['totals'];
+
+                   $pk_options = $default_options;
+
+                   $remove_primary_key_options = ['conditions', 'joins', 'order', 'limit', 'offset', 'group', 'sql', 'totals', 'having'];
+
+                   foreach ($remove_primary_key_options as $option) {
+                       if(array_key_exists($option, $pk_options))
+                           unset($pk_options[$option]);
+                   }
+
+                   if (!empty($found_ids)) {
+                       $found_objects = static::find_by_pk($found_ids,$pk_options,true);
+                   }
+                   else {
+                       $found_objects = array();
+                   }
+
+                   if (!is_array($found_objects))
+                       $found_objects = array($found_objects);
+
+                   if (!is_null($totals)) {
+                       $found_objects = new Finder(array('list' => $found_objects,'total' => $totals));
+                   }
+               }
+            }
+
+            if ($found_objects === false) {
+                $default_options = static::$finders[$name];
+
+                if (array_key_exists('ttl', $default_options )) {
+                    $cache_ttl =  $default_options['ttl'];
+                    unset($default_options['ttl']);
+                }
+                else {
+                    $cache_ttl = static::$finder_default_ttl;
+                }
+
+                if (!array_key_exists('include', $default_options )) {
+                    $default_options['include'] = static::$auto_load_associations_for_finder;
+                }
+
+                if (array_key_exists('sql', $default_options) && count($default_options['sql'])) {
+                    $sql_parameters = (isset($conditions['sql_parameters'])) ? $conditions['sql_parameters'] : $default_options['sql'][1];
+                    $sql = static::create_sql_from_finder($default_options, $filters);
+                    $found_objects = static::find_by_sql($sql, $sql_parameters, true, $default_options['include']);
+                }
+                else {
+                    $options = static::create_options_from_finder($default_options, $conditions, $filters);
+                    $found_objects = static::find('all', $options);
+                }
+
+                $object_ids = array();
+                foreach ($found_objects as $object){
+                    $object_ids[]= $object->read_attribute($object->get_primary_key(true));
+                }
+
+                $totals = null;
+
+                if (isset($filters['totals']) && $filters['totals'] == true)
+                    $totals = $found_objects->total();
+
+                $cached_array = array('ids' => $object_ids, 'totals' => $totals);
+
+                if ($cache_ttl >= 0) {
+                    if (!isset($specific_cache_id)) {
+                        $specific_cache_id = self::finder_specific_cache_id($name, $conditions, $filters);
+                    }
+
+                    Cache::set($specific_cache_id, $cached_array, $cache_ttl);
+                }
+            }
+
+            return $found_objects;
+        }
+        else {
+            throw new Exception("Finder '$name' does not exist");
+        }
+    }
+
+    /**
+     * Clears the cache of a finder for all filters
+     *
+     * @param string $name
+     * @param array $conditions
+     *
+     * @return bool
+     */
+    public static function clear_finder_cache($name, $conditions = array()){
+        if (array_key_exists($name,static::$finders)) {
+            $cache_id = self::finder_cache_id($name, $conditions);
+
+            return Cache::set($cache_id, md5(microtime()));
+        }
+        else {
+            throw new Exception("Finder '$name' does not exist");
+        }
+    }
+
+    /**
+     * Clears the cache of a finder for specific filters
+     *
+     * @param string $name
+     * @param array $conditions
+     * @param array $filters
+     *
+     * @return bool
+     */
+    public static function clear_specific_finder_cache($name, $conditions = array(), $filters = array()){
+        if (array_key_exists($name,static::$finders)) {
+            $specific_cache_id = self::finder_specific_cache_id($name, $conditions, $filters);
+
+            return Cache::delete($specific_cache_id);
+        }
+        else {
+            throw new Exception("Finder '$name' does not exist");
+        }
+    }
+
+    /**
+     * Checks if the finder with conditions and filters is cached
+     *
+     * @param string $name
+     * @param array $conditions
+     * @param array $filters
+     *
+     * @return bool
+     */
+    public static function finder_is_cached($name, $conditions = array(), $filters = array()){
+        if (array_key_exists($name,static::$finders)) {
+            $specific_cache_id = self::finder_specific_cache_id($name, $conditions, $filters);
+            return (false !== Cache::fetch($specific_cache_id));
+        }
+
+        return false;
+    }
+
+    /**
+     * Gets the group key to use with finders with the same conditions
+     *
+     * @param string $name
+     * @param array $conditions
+     *
+     * @return string
+     */
+    public static function finder_cache_group($name, $conditions) {
+        $cache_id = static::finder_cache_id($name, $conditions);
+
+        return Cache::get($cache_id, function() {
+            return md5(microtime());
+        });
+    }
+
+    /**
+     * Gets the cache key to use for a specific finder
+     *
+     * @param string $name
+     * @param array $conditions
+     * @param array $filters
+     *
+     * @return string
+     */
+    protected static function finder_specific_cache_id($name, $conditions, $filters) {
+        $group = self::finder_cache_group($name, $conditions);
+        $conditions_string = serialize(static::type_cast_values_to_string_recursive($conditions));
+        $filters_string = serialize(static::type_cast_values_to_string_recursive($filters));
+
+        return get_called_class() . $name . $group . md5($conditions_string) . md5($filters_string);
+    }
+
+    /**
+     * Gets the cache key to use for a group of finders
+     *
+     * @param string $name
+     * @param array $conditions
+     *
+     * @return string
+     */
+    protected static function finder_cache_id($name, $conditions) {
+        $conditions_string = serialize(static::type_cast_values_to_string_recursive($conditions));
+
+        return get_called_class() . $name . md5($conditions_string);
+    }
+
+    /**
+     * Creates a sql query from a finder for use in find_by_sql
+     *
+     * @param string $options
+     * @param string $limits
+     * @return string
+     */
+    public static function create_sql_from_finder($options, $limits = array()){
+        $query = $options['sql'][0];
+        $offset = 0;
+
+        if(isset($options['group']))
+            $query .= " GROUP BY {$options['group']}";
+
+        if (isset($limits['order']))
+            $query .= " ORDER BY {$limits['order']}";
+        elseif (isset($options['order']))
+            $query .= " ORDER BY {$options['order']}";
+
+        if (isset($limits['offset']))
+            $offset = $limits['offset'];
+        elseif (isset($options['offset']))
+            $offset = $options['offset'];
+
+        if (isset($limits['limit'])) {
+            if ($limits['limit'] === false) {
+                $query .= " OFFSET $offset";
+            }
+            else {
+                $query .= " LIMIT $offset,{$limits['limit']}";
+            }
+        }
+
+        return $query;
+    }
+
+    /**
+     * Creates the correct options array based on necessary conditions
+     *
+     * @param array $options
+     * @param array $additional_conditions
+     * @param array $limits
+     * @return array of options for find
+     */
+    public static function create_options_from_finder($options, $additional_conditions, $limits = array()){
+        if (array_key_exists('conditions', $options)) {
+            if (!is_hash($options['conditions'])) {
+                $string_conditions = $options['conditions'][0];
+                foreach ($additional_conditions as $attribute => $value) {
+                    // Arrays need to be used for attributes that appear multiple times in the condition string
+                    if (is_array($value) && count($value) == ($count = preg_match_all("/\b$attribute/", $string_conditions, $matches, PREG_OFFSET_CAPTURE))) {
+                        $positions = array();
+                        // Find every position of $attribute in $string_conditions
+                        foreach ($matches[0] as $match) {
+                            $positions[]= $match[1];
+                        }
+                        $i = 0;
+                        foreach ($positions as $position) {
+                            // Get the key to replace in the conditions array by counting occurrences of '?' prior
+                            // to $position in $string_conditions and adding 1
+                            $replace_key = substr_count(substr($string_conditions, 0, $position),'?') + 1;
+                            $options['conditions'][$replace_key] = $value[$i];
+                            $i++;
+                        }
+                    }
+                    elseif (isset($value)) {
+                        $number = preg_match("/\b$attribute/", $string_conditions, $matches, PREG_OFFSET_CAPTURE);
+                        $occurrence = $matches[0][1];
+
+                        if($occurrence == 0) {
+                            $occurrence = 1;
+                        }
+
+                        $replace_key = substr_count(substr($string_conditions, 0, $occurrence - 1), '?') + 1;
+                        $options['conditions'][$replace_key] = $value;
+                    }
+                }
+            }
+            else {
+                $options['conditions'] = array_merge($options['conditions'],$additional_conditions);
+            }
+        }
+
+        foreach ($limits as $key => $value) {
+            if ($key == 'limit' && $value === false) {
+                unset($options[$key]);
+            }
+            elseif (isset($value)) {
+                $options[$key] = $value;
+            }
+        }
+
+        return $options;
+    }
+
+    /**
+     * Takes all values in an array and typecasts to string
+     *
+     * @return array
+     **/
+    public static function type_cast_values_to_string_recursive($arr) {
+        foreach ($arr as $key => $value) {
+            if (is_array($value)) {
+                $arr[$key] = static::type_cast_values_to_string_recursive($value);
+            }
+            else {
+                $arr[$key] = (string) $value;
+            }
+        }
+
+        return $arr;
     }
 };
 ?>
